4e6b7b935ffff0d420fd2caa3c768b6cd1376745
Steven-Tang-rong <gzodc0045288@gmail.com>
2021-02-23 20:29:12 +0800

    Initial Commit

---
+++ UploadData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UploadData
+//
+//  Created by TANG,QI-RONG on 2021/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
